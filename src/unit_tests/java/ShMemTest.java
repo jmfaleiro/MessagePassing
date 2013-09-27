336c4302f7b9a43ace3196f7f44eca6cd568bb6f
Jose Faleiro <faleiro.jose.manuel@gmail.com>
2013-09-27 14:28:11 -0400

    Add some more unit tests for diffs in Java.

--- src/unit_tests/java/ShMemTest.java
+++ src/unit_tests/java/ShMemTest.java
@@ -2,6 +2,13 @@ package unit_tests.java;
 
 
 import static org.junit.Assert.*;
+
+import java.util.Iterator;
+
+import org.codehaus.jackson.JsonNode;
+import org.codehaus.jackson.node.ArrayNode;
+import org.json.simple.JSONObject;
+
 import mp.java.ITimestamp.Comparison;
 import mp.java.ShMem;
 import mp.java.ShMemObject;
@@ -58,6 +65,98 @@ public class ShMemTest {
 		assertEquals(VectorTimestamp.Compare(start_time,  temp),
 					 Comparison.NONE);
 					 
+	}
+	
+	private void standardInit() {
+		ShMem.Init(0);
+		ShMem.Start();
+		
+		ShMemObject first_obj = new ShMemObject();
+		first_obj.put("Yale",  "University");
+		ShMem.s_state.put("name", first_obj);
+		
+		VectorTimestamp.IncrementLocal(ShMemObject.s_now);
+		first_obj.put("CS",  "Watson");
+		
+		ShMemObject.s_now[0] = 2;
+		ShMemObject.s_now[1] = 2;
+		
+		ShMemObject second_obj = new ShMemObject();
+		second_obj.put("gah",  "blah");
+		first_obj.put("second",  second_obj);
+	}
+	
+	private void checkGah(JsonNode diff) {
+		JsonNode name_wrapper = diff.get("name");
+		ArrayNode name_timestamp = (ArrayNode)name_wrapper.get("shmem_timestamp");
+		int[] temp = {2,2,0,0};
+		assertEquals(VectorTimestamp.CompareWithSerializedTS(temp,  name_timestamp),
+					 Comparison.EQ);
+		JsonNode second = name_wrapper.get("value");
+		JsonNode second_wrapper = second.get("second");
+		ArrayNode second_timestamp = (ArrayNode)second_wrapper.get("shmem_timestamp");
+		assertEquals(VectorTimestamp.CompareWithSerializedTS(temp, second_timestamp),
+					 Comparison.EQ);
+		JsonNode gah = second_wrapper.get("value");
+		JsonNode gah_wrapper = gah.get("gah");
+		ArrayNode gah_timestamp = (ArrayNode)gah_wrapper.get("shmem_timestamp");
+		assertEquals(VectorTimestamp.CompareWithSerializedTS(temp,  gah_timestamp), 
+					 Comparison.EQ);
+		String blah_string = gah_wrapper.get("value").getTextValue();
+		assertEquals(blah_string.equals("blah"), true);
+	}
+	
+	private void checkWatson(JsonNode diff) {
+		JsonNode name_wrapper = diff.get("name");
+		JsonNode cs_wrapper = name_wrapper.get("value").get("CS");
+		ArrayNode cs_timestamp = (ArrayNode)cs_wrapper.get("shmem_timestamp");
+		int[] temp = {2,0,0,0};
+		assertEquals(VectorTimestamp.CompareWithSerializedTS(temp,  cs_timestamp),
+					 Comparison.EQ);
+		assertEquals(cs_wrapper.get("value").getTextValue().equals("Watson"), true);
+	}
+	
+	private void checkYale(JsonNode diff) {
+		JsonNode name_wrapper = diff.get("name");
+		JsonNode yale_wrapper = name_wrapper.get("value").get("Yale");
+		ArrayNode yale_timestamp = (ArrayNode)yale_wrapper.get("shmem_timestamp");
+		int[] temp = {1,0,0,0};
+		assertEquals(VectorTimestamp.CompareWithSerializedTS(temp,  yale_timestamp),	
+					 Comparison.EQ);
+		String yale_value = yale_wrapper.get("value").getTextValue();
+		assertEquals(yale_value.equals("University"), true);
+	}
+	
+	@org.junit.Test
+	public void testDiff() {
+		standardInit();
+		
+		int[] temp = {2,3,0,0};
+		JsonNode diff = ShMemObject.get_diff_tree(ShMem.s_state, temp);
+		int key_count = 0;
+		
+		Iterator<String> key_iter =  diff.getFieldNames();
+		while (key_iter.hasNext()) {
+			key_count += 1;
+		}
+		assertEquals(key_count, 0);
+		
+		temp[1] = 1;
+		diff = ShMemObject.get_diff_tree(ShMem.s_state, temp);
+		checkGah(diff);
+		
+		temp[0] = 1;
+		temp[1] = 0;
+		diff = ShMemObject.get_diff_tree(ShMem.s_state,  temp);
+		checkGah(diff);
+		checkWatson(diff);
+		
+		temp[0] = 0;
+		temp[1] = 0;
+		diff = ShMemObject.get_diff_tree(ShMem.s_state, temp);
+		checkGah(diff);
+		checkWatson(diff);
+		checkYale(diff);
 	}
 	
 	@org.junit.Test
