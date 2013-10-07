568b885132532a584674d8b8f130a195ab57093a
John <john@smorz.cs.yale.edu.fake>
2013-10-07 01:09:18 -0400

    worker successfully cleans png! - mat doesn't work for pdfs

--- src/archivist/kirigami/python/worker.py
+++ src/archivist/kirigami/python/worker.py
@@ -31,14 +31,21 @@ def findfiles(shmem):
             else:
 		# file
 		# check if this worker should clean it (depends on filetype)
-		if key.endswith('.pdf'):
+		if key.endswith('.png'):
 			# write to file
-			filename = key
-			g = open('weak-test.pdf','wb')
+			filename = 'weak-test.png'
+			g = open(filename,'wb')
 	    		g.write(base64.b64decode(value))
 	    		g.close()
 			# clean the file
-			#os.system('../../../../../mat/mat ' + filename)
+			os.system('../../../../../mat/mat ' + filename)
+			
+			g = open(filename,'rb')
+	    		bytes = g.read()
+	      	        bytes = base64.b64encode(bytes)
+	    		g.close()
+			# update the value for that key in shmem
+			shmem.put_simple(key,bytes)
 
 			# then read file back into shmem object
                 	ret[key] = value
