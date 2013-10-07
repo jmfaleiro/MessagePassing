44bfb1b32a30943bafb0d3e3572e89fa6360f72d
Jose Faleiro <faleiro.jose.manuel@gmail.com>
2013-10-07 17:08:29 -0400

    Make appender read tweets from disk.

--- src/archivist/kirigami/java/Appender.java
+++ src/archivist/kirigami/java/Appender.java
@@ -1,117 +1,86 @@
 package archivist.kirigami.java;
 
-import twitter4j.*;
-import twitter4j.conf.ConfigurationBuilder;
-
-import java.util.*;
+import java.io.BufferedReader;
+import java.io.FileReader;
 
 import org.codehaus.jackson.map.ObjectMapper;
-import org.codehaus.jackson.node.ObjectNode;
+import org.codehaus.jackson.node.ArrayNode;
 
 import mp.java.*;
+import mp.java.ShMemObject.MergeException;
 
 public class Appender {
 	
-	private static int pages = 15;
+	private final String m_input_file;
+	private final int m_batch_size;
+	private final int m_num_nodes;
 	
-	private long since_id = 0;
-	private String query_string;
-	
-	private Twitter twitter;
-	
-	public Appender(String query_string) { 
-		
-		ConfigurationBuilder cb = new ConfigurationBuilder();
-		cb.setDebugEnabled(true)
-		  .setOAuthConsumerKey("Z5lqe8Rf3k9vJ2Kk7XIRw")
-		  .setOAuthConsumerSecret("4V4XYAC4Ujp8KlRKh65ZlvSSFQh4aDuiZqxWDo4U")
-		  .setOAuthAccessToken("253532484-12uMOisz0lm48KKhxKMIZFp0giMTgsil9E4QihNg")
-		  .setOAuthAccessTokenSecret("zbO9LGmYWq32DnuVAnBAGFJfCyUVBtlyI3Mtd6GKxUU");
-		
-		this.query_string = query_string;
-		
-		twitter = new TwitterFactory(cb.build()).getInstance();
+	public Appender(String input_file, int batch_size, int num_nodes) { 
+		m_input_file = input_file;
+		m_batch_size = batch_size;
+		m_num_nodes = num_nodes;
 	}
 	
-	public int Search() throws TwitterException {
-		String next_tweet_string;
-		int next_tweet = 0;
-		
-		ObjectMapper mapper = new ObjectMapper();
-		
-		ShMemObject tweets = new ShMemObject();
-		Query query = new Query(query_string);
-		query.setCount(100);
-		query.setSinceId(this.since_id);
-		QueryResult result;
-		
-		int i = 0;
-		do {
-			if (i >= pages)
-				break;
-			
-			result = twitter.search(query);
-			List<Status> temp = result.getTweets();
-			this.since_id = result.getMaxId()+1;
-			
-			
-			for (Status s : temp) {
-				
-				this.since_id = s.getId();
-				String date_string = s.getCreatedAt().toString();
-				String user_string = s.getUser().getName();
-				String tweet_string = s.getText();
-				String source_string = s.getSource();
-				
-				ObjectNode cur_tweet = mapper.createObjectNode();
-				cur_tweet.put("created_at",  date_string);
-				cur_tweet.put("user",  user_string);
-				cur_tweet.put("text",  tweet_string);
-				cur_tweet.put("source", source_string);
-				
-				next_tweet_string = String.valueOf(next_tweet);
-				tweets.put(next_tweet_string, cur_tweet);
-				next_tweet += 1;
-			}
-			++i;
-			
-		} while((query = result.nextQuery()) != null);
 	
-		
-		// Add dummy state for volume and user aggregations to use. 
-		ShMem.s_state.put("volume-aggregate",  new ShMemObject());
-		ShMem.s_state.put("word-aggregate",  new ShMemObject());
-		ShMem.s_state.put("user-aggregate",  new ShMemObject());
-		ShMem.s_state.put("url-aggregate",  new ShMemObject());
-		ShMem.s_state.put("source-aggregate", new ShMemObject());
-		ShMem.s_state.put("retweet-aggregate",  0);
-		ShMem.s_state.put("tweets", tweets);
-		ShMem.s_state.put("search_term",  query_string);
-		
-		for (i = 1; i < 7; ++i) {
+	public void ReleaseAll() {
+		for (int i = 1; i < m_num_nodes; ++i) {
 			ShMem.Release(i);
 		}
-		
-		for (i = 1; i < 7; ++i) {
+	}
+	
+	public void AcquireAll() {
+		for (int i = 1; i < m_num_nodes; ++i) {
 			try {
 				ShMem.Acquire(i);
 			}
-			catch (ShMemObject.MergeException e) {
-				e.printStackTrace(System.err);
-				System.out.println("Merge failed!");
+			catch (MergeException e) {
+				System.out.println("Didn't expect merge exception!");
 				System.exit(-1);
 			}
 		}
-		return 0;
+	}
+	
+	public void Process() {
+		ObjectMapper mapper = new ObjectMapper();
+		ArrayNode epoch_tweets = mapper.createArrayNode();
+		
+		try {
+			FileReader reader = new FileReader("test_cases/" + m_input_file);
+	    	@SuppressWarnings("resource")
+			BufferedReader file = new BufferedReader(reader);
+	    	
+	    	String line = null;
+	    	while (true) {
+	    		
+		    	for (int i = 0; i < m_batch_size; ++i) {
+		    		line = file.readLine();
+		    		if (line == null) {
+		    			break;
+		    		}
+		    		epoch_tweets.add(mapper.readTree(line));
+		    	}
+		    	ShMem.s_state.put("tweets",  epoch_tweets);
+		    	AcquireAll();
+		    	ReleaseAll();
+		    	
+		    	if (line == null) {
+		    		break;
+		    	}
+	    	}
+	    	
+	    	AcquireAll();
+		}
+		catch(Exception e) {
+			e.printStackTrace(System.out);
+			System.exit(-1);
+		}
 	}
 	
 	
-	public static void main(String [] args) throws TwitterException {
+	public static void main(String [] args)  {
 		ShMem.Init(Integer.parseInt(args[0]));
 		ShMem.Start();
-		Appender blah;
-		
-		blah = new Appender("xbox"); 
-		blah.Search();
+		Appender blah = new Appender(args[0], Integer.parseInt(args[1]), Integer.parseInt(args[2]));
+		blah.Process();
 	}
 }
