384fb3f2fcb8d3b723ce56171ac12fa53a808f3b
Jose Faleiro <faleiro.jose.manuel@gmail.com>
2013-10-07 00:18:54 -0400

    convert recieved input to unicode.

--- src/mp/python/ShMemAcquirer.py
+++ src/mp/python/ShMemAcquirer.py
@@ -47,7 +47,7 @@ class ShMemAcquirer:
             # Close the resource. 
             clientsocket.close()
             # Do some deserialization. 
-            recvd_obj = json.loads(msg)
+            recvd_obj = json.loads(unicode(msg))
             msg = recvd_obj['argument']
             sender = recvd_obj['releaser']  
  
